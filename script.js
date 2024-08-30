d417260fe2ad5b277e0724a9c6b0fb6a01f6f995
Vincent CHEN <vincentchen.cp@gmail.com>
2024-08-30 17:29:30 +0200

    Le status de la page change correctement de 'start' à 'ingame'

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 /* variable needed to made the game work */
 let page = 'start' // the button need to display 'start' if it's the first time game
 let playerTurn = '1' // the game start with the first player
+const cells = [...document.querySelectorAll('.cell')]
+const winConditions =  [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+let options = ["", "", "", "", "", "", "", "", ""]
 
  /* store a button that will show 'start' or 'restart' depending on if it's the first game or if the players want to replay */
-let button = document.createElement('button')
+const button = document.createElement('button')
 
 /* displaying 'Start' or 'Restart' if the user already play */
 if (page === 'start'){
@@ -31,4 +43,17 @@ button.style.fontSize = "20px"
 button.style.backgroundColor = "#82B6D9"
 button.style.boxShadow = "10px 10px #00000040"
 button.style.fontWeight = "bold"
+button.style.cursor = "pointer"
 
+function initalizeGame() {
+    
+}
+
+function cellClicked() {
+    
+}
+
+button.addEventListener('click', (e) => {
+    page = 'ingame'
+    initalizeGame()
+})
